a4663202833da9ec0eab7ae6896c3640d1ac69cc
Mudit Ameta <zeusdeux@gmail.com>
2018-11-26 21:36:06 +0530

    Add multiple (es5-cjs, es5-esm, es9-esm) build targets
    This commit adds the following:
    
    - multiple transpiled targets (es5-cjs, es5-esm, es9-esm)
      - main -> es5-cjs
      - module -> es5-esm
      - es/main-es -> es9-esm
    - an `engines.node` key is added and set to >=10 as that supports 100%
    of ES2018 as per https://node.green/#ES2018
    - runtime envrionment is assumed to support es2018 features and thus
    ES2018 lib is included in the base config
    - an `npm` `prepare` script is provided to build artifacts
    - a `tsconfig.json` symlink is provided so that `tsserver` works as
    expected and thus your editor of choice works as expected
    
    Caveats:
    
    - only TS syntax is transpiled therefore no polyfills are provided and
    must be handled downstream

--- lib/__tests__/index-test.ts
+++ lib/__tests__/index-test.ts
@@ -1,7 +1,17 @@
-import sum from '../'
+import sum, { padString } from '../'
 
-describe('index#default', () => {
-  it('should return the sum of given input numbers', () => {
-    expect(sum(2, 3)).toBe(5)
+describe('index', () => {
+  describe('#default', () => {
+    it('should return the sum of given input numbers', () => {
+      expect(sum(2, 3)).toBe(5)
+    })
+  })
+  describe('#padString', () => {
+    it('should pad a string evenly on both ends when given an even max length', () => {
+      expect(padString('12', 4, 'o')).toBe('oo12oo')
+    })
+    it('should pad a string unevenly with preference given to the end when given an odd max length', () => {
+      expect(padString('12', 5, 'o')).toBe('oo12ooo')
+    })
   })
 })
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,28 @@
+// convoluted example to show different compilation targets
+// are respected
 export default function(a: number, b: number): number {
-  return a + b
+  const id = (x: any) => x
+  return id(a) + id(b)
+}
+
+// examples below to check if the correct libs are loaded up
+// in your respective editors
+
+// array.includes is from lib.es2016
+export function includes<T>(v: T, arr: T[]): boolean {
+  return arr.includes(v)
+}
+
+// padStart and padEnd are from lib.es2017
+export function padString(inputString: string, maxLength: number, fillWith?: string): string {
+  const strLen = inputString.length
+
+  return inputString
+    .padStart(strLen + Math.floor(maxLength / 2), fillWith)
+    .padEnd(strLen + maxLength, fillWith)
+}
+
+// Promise.finally is from lib.2018
+export function addFinally(promise: Promise<number>, cb: () => void) {
+  return promise.finally(cb)
 }
