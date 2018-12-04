539b258b752b8ed4c8bc6680684634b7d9c3b377
Fazouane Marouane <fazouanem3@gmail.com>
2018-12-04 23:45:06 +0100

    Switch Tokenizer to class

--- lib/index.ts
+++ lib/index.ts
@@ -3,99 +3,101 @@ import atxHeading from './atx-heading'
 import paragraph from './paragraph'
 import block from './block'
 
-Tokenizer.prototype.write = write
-Tokenizer.prototype.current = current
-Tokenizer.prototype.now = now
-Tokenizer.prototype.consume = consume
-Tokenizer.prototype.reconsume = reconsume
-Tokenizer.prototype.next = next
-Tokenizer.prototype.switch = switchContext
-Tokenizer.prototype.block = block
-Tokenizer.prototype.atxHeading = atxHeading
-Tokenizer.prototype.paragraph = paragraph
+export class Tokenizer {
+  data = ''
+  line = 1
+  column = 1
+  virtualColumn = 1
+  offset = 0
+  tabSize = 2
 
-export function Tokenizer() {
-  this.data = ''
-  this.line = 1
-  this.column = 1
-  this.virtualColumn = 1
-  this.offset = 0
-  this.switch('block')
-}
+  context: string = ''
+  stateHandlers: any
+  contextInfo = {}
+  state: string = ''
 
-function switchContext(name) {
-  this.context = name
-  this.stateHandlers = this[name]
-  this.contextInfo = {}
-  this.state = 'START_STATE'
-}
-
-function write(chunk) {
-  var self = this
-
-  self.data += chunk
-
-  while (self.offset <= self.data.length) {
-    self.next()
-  }
-}
-
-function next() {
-  var self = this
-  var fn
-
-  fn = self.stateHandlers[self.state]
-
-  if (!fn) {
-    throw new Error('Cannot handle `' + self.context + '.' + self.state + '`')
+  constructor() {
+    this.switch('block')
   }
 
-  fn.call(self, self.current())
-}
+  write(chunk) {
+    var self = this
 
-function reconsume(state) {
-  this.state = state
-  this.next()
-}
+    self.data += chunk
 
-function consume() {
-  var self = this
-  var code = self.current()
-  var tabSize = self.tabSize
-
-  if (code === c.eof || code === c.lineFeed) {
-    self.line++
-    self.column = 0
-  } else if (code === c.tab) {
-    self.virtualColumn = Math.floor(self.virtualColumn / tabSize) * tabSize + tabSize
+    while (self.offset <= self.data.length) {
+      self.next()
+    }
   }
 
-  console.log('consume: %s', self.state, self.now(), [String.fromCharCode(code)])
+  current() {
+    var self = this
+    var length = self.data.length
+    var offset = self.offset
 
-  self.column++
-  self.offset++
-}
+    if (offset > length) {
+      throw new Error('Cannot read after end')
+    }
 
-function current() {
-  var self = this
-  var length = self.data.length
-  var offset = self.offset
+    // Use `null` instead of NaN for EOF code.
+    if (length === offset) {
+      return null
+    }
 
-  if (offset > length) {
-    throw new Error('Cannot read after end')
+    // Use `�` instead of `\0` (https://spec.commonmark.org/0.28/#insecure-characters).
+    return self.data.charCodeAt(offset) || c.replacementCharacter
   }
 
-  // Use `null` instead of NaN for EOF code.
-  if (length === offset) {
-    return null
+  now() {
+    var self = this
+
+    return { line: self.line, column: self.column, offset: self.offset }
   }
 
-  // Use `�` instead of `\0` (https://spec.commonmark.org/0.28/#insecure-characters).
-  return self.data.charCodeAt(offset) || c.replacementCharacter
-}
+  consume() {
+    var self = this
+    var code = self.current()
+    var tabSize = self.tabSize
 
-function now() {
-  var self = this
+    if (code === c.eof || code === c.lineFeed) {
+      self.line++
+      self.column = 0
+    } else if (code === c.tab) {
+      self.virtualColumn = Math.floor(self.virtualColumn / tabSize) * tabSize + tabSize
+    }
 
-  return { line: self.line, column: self.column, offset: self.offset }
+    console.log('consume: %s', self.state, self.now(), [String.fromCharCode(code)])
+
+    self.column++
+    self.offset++
+  }
+
+  reconsume(state) {
+    this.state = state
+    this.next()
+  }
+
+  next() {
+    var self = this
+    var fn
+
+    fn = self.stateHandlers[self.state]
+
+    if (!fn) {
+      throw new Error('Cannot handle `' + self.context + '.' + self.state + '`')
+    }
+
+    fn.call(self, self.current())
+  }
+
+  switch(name) {
+    this.context = name
+    this.stateHandlers = this[name]
+    this.contextInfo = {}
+    this.state = 'START_STATE'
+  }
+
+  block = block
+  atxHeading = atxHeading
+  paragraph = paragraph
 }
