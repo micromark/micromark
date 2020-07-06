fe2734b1aad9afdc39263061e4999d62e5fd3f67
Titus Wormer <tituswormer@gmail.com>
2020-07-06 09:56:20 +0200

    Add character reference test for nul

--- test/io/misc/nul.js
+++ test/io/misc/nul.js
@@ -10,5 +10,14 @@ test('nul', function (t) {
     'should replace `\\0` with a replacement characters (`�`)'
   )
 
+  t.equal(m('&#0;'), '<p>�</p>', 'should replace NUL in a character reference')
+
+  // Markdown would never support this, as escapes only work on ASCII punctuation.
+  t.equal(
+    m('\\0'),
+    '<p>\\0</p>',
+    'should not support NUL in a character escape'
+  )
+
   t.end()
 })
