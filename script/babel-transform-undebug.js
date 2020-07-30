1e2b3c9d353cb253001ebf451a44bcdef3406818
Titus Wormer <tituswormer@gmail.com>
2020-07-30 18:55:25 +0200

    Refactor to externalize functions

--- script/babel-transform-undebug.js
+++ script/babel-transform-undebug.js
@@ -6,32 +6,38 @@ module.exports = undebug
 function undebug() {
   return {
     visitor: {
-      AssignmentExpression(nodePath) {
-        if (
-          nodePath.equals('operator', '=') &&
-          isRequireDebug(nodePath.get('left'), nodePath.get('right'))
-        ) {
-          nodePath.remove()
-        }
-      },
-      VariableDeclarator(nodePath) {
-        if (isRequireDebug(nodePath.get('id'), nodePath.get('init'))) {
-          nodePath.remove()
-        }
-      },
-      CallExpression(nodePath) {
-        var callee = nodePath.get('callee')
-
-        if (
-          nodePath.parentPath &&
-          nodePath.parentPath.isExpressionStatement() &&
-          callee.isIdentifier() &&
-          callee.equals('name', 'debug')
-        ) {
-          nodePath.remove()
-        }
-      }
+      AssignmentExpression: assignmentExpression,
+      CallExpression: callExpression,
+      VariableDeclarator: variableDeclarator
     }
+  }
+}
+
+function assignmentExpression(nodePath) {
+  if (
+    nodePath.equals('operator', '=') &&
+    isRequireDebug(nodePath.get('left'), nodePath.get('right'))
+  ) {
+    nodePath.remove()
+  }
+}
+
+function callExpression(nodePath) {
+  var callee = nodePath.get('callee')
+
+  if (
+    nodePath.parentPath &&
+    nodePath.parentPath.isExpressionStatement() &&
+    callee.isIdentifier() &&
+    callee.equals('name', 'debug')
+  ) {
+    nodePath.remove()
+  }
+}
+
+function variableDeclarator(nodePath) {
+  if (isRequireDebug(nodePath.get('id'), nodePath.get('init'))) {
+    nodePath.remove()
   }
 }
 
