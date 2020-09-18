b0e4c98720288b0b4593f484c0e1f072eefe2baf
Christian Murphy <christian.murphy.42@gmail.com>
2020-09-18 08:19:32 -0700

    test: variations of frontmatter and gfm

--- fuzzer.js
+++ fuzzer.js
@@ -1,13 +1,49 @@
 const micromark = require('./index')
+const frontmatter = require('micromark-extension-frontmatter')
+const gfmSyntax = require('micromark-extension-gfm')
+const gfmHtml = require('micromark-extension-gfm/html')
 
 function fuzz(buf) {
   try {
+    // // commonmark buffer without html
     micromark(buf)
+
+    // // commonmark buffer with html
+    micromark(buf, 'utf-8', {
+      allowDangerousHtml: true,
+      allowDangerousProtocol: true
+    })
+
+
+    micromark(buf, 'utf-8', {
+      extensions: [frontmatter()]
+    })
+
+    micromark(buf, 'utf-8', {
+      extensions: [frontmatter()],
+      allowDangerousHtml: true,
+      allowDangerousProtocol: true
+    })
+
+    // gfm buffer without html
+    micromark(buf, 'utf-8', {
+      extensions: [gfmSyntax()],
+      htmlExtensions: [gfmHtml]
+    })
+
+    // gfm buffer with html
+    micromark(buf, 'utf-8', {
+      allowDangerousHtml: true,
+      allowDangerousProtocol: true,
+      extensions: [gfmSyntax()],
+      htmlExtensions: [gfmHtml]
+    })
   } catch (e) {
-    if (e.message.indexOf('URI malformed') !== -1) {
-    } else {
-      throw e
-    }
+    // if (e.message.indexOf('URI malformed') !== -1) {
+    // } else {
+    //   throw e
+    // }
+    throw e
   }
 }
 
