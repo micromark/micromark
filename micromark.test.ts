b839f9ecad3b315beeda3c3d1668ad080c3ba796
ocavue <ocavue@gmail.com>
2020-10-07 20:00:19 +0800

    add more test

--- micromark.test.ts
+++ micromark.test.ts
@@ -1,20 +1,62 @@
-import {EventEmitter} from 'events'
+// This file is for https://github.com/microsoft/dtslint .
+// Tests are type-checked, but not run.
+
 import micromarkBuffer from 'micromark'
 import micromarkStream from 'micromark/stream'
 
-// $ExpectType string
-const html: string = micromarkBuffer('# text **strong**', {
-  allowDangerousHtml: true,
-  allowDangerousProtocol: true,
-  extensions: []
-})
+function testBuffer() {
+  const raw = '# text **strong**'
 
-// $ExpectType EventEmitter
-const emitter: EventEmitter = micromarkStream({
-  allowDangerousHtml: true,
-  allowDangerousProtocol: true
-})
+  // $ExpectType string
+  micromarkBuffer(raw)
 
-if (html || emitter) {
-  // Do nothing
+  // $ExpectType string
+  micromarkBuffer(raw, {
+    defaultLineEnding: 'utf-8',
+    allowDangerousHtml: true,
+    allowDangerousProtocol: true,
+    extensions: [],
+    htmlExtensions: []
+  })
+
+  // $ExpectType string
+  micromarkBuffer(raw, {allowDangerousHtml: true})
+
+  // $ExpectType string
+  micromarkBuffer(Buffer.alloc(8), {allowDangerousHtml: true})
+
+  // $ExpectType string
+  micromarkBuffer(Buffer.alloc(8), 'utf-8', {allowDangerousHtml: true})
+
+  // $ExpectError
+  micromarkBuffer(Buffer.alloc(8), 'this_encoding_does_not_exist', {
+    allowDangerousHtml: true
+  })
+
+  // $ExpectError
+  micromarkBuffer(1234, {allowDangerousHtml: true})
+
+  // $ExpectError
+  micromarkBuffer(raw, 'utf8', {this_is_not_a_valid_option: 0})
+
+  // $ExpectError
+  micromarkBuffer(raw, 'utf8', {defaultLineEnding: '????'})
 }
+
+function testStream() {
+  // $ExpectType EventEmitter
+  micromarkStream({
+    defaultLineEnding: 'utf-8',
+    allowDangerousHtml: true,
+    allowDangerousProtocol: true,
+    extensions: [],
+    htmlExtensions: []
+  })
+}
+
+function main() {
+  testBuffer()
+  testStream()
+}
+
+main()
