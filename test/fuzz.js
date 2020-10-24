ef504882238cb0352e7a5bf934a9fbaf58069557
Titus Wormer <tituswormer@gmail.com>
2020-10-24 13:02:27 +0200

    Update fuzz testing to allow more cycles

--- test/fuzz.js
+++ test/fuzz.js
@@ -3,10 +3,4 @@ import m from '..'
 export function fuzz(buf) {
   // Buffer.
   m(buf)
-
-  // An encoding.
-  m(buf, 'ascii')
-
-  // Options.
-  m(buf, {allowDangerousHtml: true, allowDangerousProtocol: true})
 }
